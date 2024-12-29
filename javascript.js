be27094a37a8162dd6a1c857a00a1ea8cb70b001
Leonardo <leonardoms.2010@hotmail.com>
2024-12-29 15:19:22 -0300

    Here is the development of function getWeather() that make possible get the API data based on what the user types

--- javascript.js
+++ javascript.js
@@ -3,28 +3,38 @@
 const apiKey = "af19fa1cb440a47653de8fd5d527fc63";
 const apiUrl =  "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
-async function verifyWeather(){
+async function getWeather(){
+    const input = document.getElementById('navText').value;
+    alert(input);
+    await verifyWeather(input);
+
+
+    // document.getElementById('navButton').addEventListener("click", function(){
+    //     input = document.getElementById('navText').innerHTML;
+    //     alert("Input1"+input);
+    // });
+    // alert("Input2"+ input);
+    // verifyWeather(input);
+}
+
+async function verifyWeather(str){
     try {
-        var city = "moscow";
-        const response = await fetch(apiUrl + "&q="+city + '&appid='+apiKey );
+        let response = await fetch(apiUrl + "&q="+str+ "&appid="+apiKey );
         var data = await response.json();
-        console.log(data);
-        var name = data.name
+        var name = data.name;
         var temp = data.main.temp + "°C";
         var humidity = data.main.humidity + "%";
         var windSpeed = data.wind.speed + "km/h";
-        var weather = data.weather[0].main
-        console.log(name+ "\n" + weather +"\n" + temp + "\n" + humidity + "\n" + windSpeed);
-        
+        var weather = data.weather[0].main;
+        alert(name+ "\n" + weather +"\n" + temp + "\n" + humidity + "\n" + windSpeed);
+
     } catch (error) {
         console.log(error);
     }
     
 }
 
-function main(){
-    
-}
+
 
 
 
